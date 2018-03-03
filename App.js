dcdea1cefbcc27e608dcc5c4cd4f98205f1fa7ab
Albert Chung <albertc435@gmail.com>
2018-03-03 16:29:34 -0500

    merged2

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header, Calendar, Agenda, InfiniteCalendar } from './src/components';
-
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <InfiniteCalendar />
-        <Agenda />
-      </View>
-    );
-  }
-}
-=======
 import App from './app/index';
 
 export default App;
->>>>>>> origin/navin_folderless
