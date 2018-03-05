b030d9015ade70594288a949116f7a21519f0f40
navinramsaroop <nr292@cornell.edu>
2018-03-05 09:26:50 -0500

    fixed calendar style

--- app/components/Calendar/styles/SliderEntry.style.js
+++ app/components/Calendar/styles/SliderEntry.style.js
@@ -1,14 +1,16 @@
 import { StyleSheet, Dimensions, Platform } from 'react-native';
 
 const IS_IOS = Platform.OS === 'ios';
-const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
+  'window'
+);
 
-function wp (percentage) {
-    const value = (percentage * viewportWidth) / 100;
-    return Math.round(value);
+function wp(percentage) {
+  const value = percentage * viewportWidth / 100;
+  return Math.round(value);
 }
 
-const slideHeight = viewportHeight * 0.50;
+const slideHeight = viewportHeight * 0.5;
 const slideWidth = wp(95);
 const itemHorizontalMargin = wp(2);
 
@@ -18,49 +20,49 @@ export const itemWidth = slideWidth + itemHorizontalMargin * 2;
 const entryBorderRadius = 8;
 
 export default StyleSheet.create({
-    slideInnerContainer: {
-        flex: 1,
-        width: itemWidth,
-        height: slideHeight,
-        paddingHorizontal: itemHorizontalMargin,
-        paddingBottom: 18 // needed for shadow
-    },
-    imageContainer: {
-        flex: 1,
-        marginBottom: IS_IOS ? 0 : -1, // Prevent a random Android rendering issue
-        backgroundColor: 'white',
-        borderTopLeftRadius: entryBorderRadius,
-        borderTopRightRadius: entryBorderRadius
-    },
-    image: {
-        ...StyleSheet.absoluteFillObject,
-        resizeMode: 'cover',
-        borderRadius: IS_IOS ? entryBorderRadius : 0,
-        borderTopLeftRadius: entryBorderRadius,
-        borderTopRightRadius: entryBorderRadius
-    },
-    // image's border radius is buggy on iOS; let's hack it!
-    radiusMask: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: entryBorderRadius,
-        backgroundColor: 'white'
-    },
-    textContainer: {
-        justifyContent: 'center',
-        paddingTop: 20 - entryBorderRadius,
-        paddingBottom: 20,
-        paddingHorizontal: 16,
-        backgroundColor: 'white',
-        borderBottomLeftRadius: entryBorderRadius,
-        borderBottomRightRadius: entryBorderRadius
-    },
-    titleEven: {
-        color: 'white'
-    },
-    subtitleEven: {
-        color: 'rgba(255, 255, 255, 0.7)'
-    }
+  slideInnerContainer: {
+    flex: 1,
+    width: itemWidth,
+    height: slideHeight,
+    paddingHorizontal: itemHorizontalMargin,
+    paddingBottom: 18 // needed for shadow
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: IS_IOS ? 0 : -1, // Prevent a random Android rendering issue
+    backgroundColor: 'white',
+    borderTopLeftRadius: entryBorderRadius,
+    borderTopRightRadius: entryBorderRadius
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
+    borderRadius: IS_IOS ? entryBorderRadius : 0,
+    borderTopLeftRadius: entryBorderRadius,
+    borderTopRightRadius: entryBorderRadius
+  },
+  // image's border radius is buggy on iOS; let's hack it!
+  radiusMask: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: entryBorderRadius,
+    backgroundColor: 'white'
+  },
+  textContainer: {
+    justifyContent: 'center',
+    paddingTop: 20 - entryBorderRadius,
+    paddingBottom: 20,
+    paddingHorizontal: 16,
+    backgroundColor: 'white',
+    borderBottomLeftRadius: entryBorderRadius,
+    borderBottomRightRadius: entryBorderRadius
+  },
+  titleEven: {
+    color: 'white'
+  },
+  subtitleEven: {
+    color: 'rgba(255, 255, 255, 0.7)'
+  }
 });
--- app/components/screens/Calendar.js
+++ app/components/screens/Calendar.js
@@ -1,71 +1,91 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Dimensions, ActivityIndicator, FlatList, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet
+} from 'react-native';
 import { itemWidth } from '../Calendar/styles/SliderEntry.style';
-import {SliderEntry} from '../Calendar';
+import { SliderEntry } from '../Calendar';
+import Agenda from '../Agenda/Agenda';
 
 class Calendar extends Component {
-
   constructor(props) {
     super(props);
-    data = []
-    for (i = -500; i < 500; i++){
-      data.push({key: i})
+    data = [];
+    for (i = -500; i < 500; i++) {
+      data.push({ key: i });
     }
     this.state = {
       last: 499,
       data: data
-    }
+    };
   }
-  getItemLayout = (data, index) => (
-    {length: itemWidth, offset: itemWidth * index, index}
+  getItemLayout = (data, index) => ({
+    length: itemWidth,
+    offset: itemWidth * index,
+    index
+  });
 
-  )
-
-  _renderItem = ({item}) => (
+  _renderItem = ({ item }) => (
     /*<Text> {item.key} </Text>*/
-    <SliderEntry data = {new Date((new Date()).getFullYear(), (new Date()).getMonth()+ item.key, 0)} />
+    <SliderEntry
+      data={
+        new Date(new Date().getFullYear(), new Date().getMonth() + item.key, 0)
+      }
+    />
   );
 
   _loadMore = () => {
-    newData = []
-    current = this.state.last
-    for (i = 1; i < 20; i++){
-      newData.push({key: i + current})
+    newData = [];
+    current = this.state.last;
+    for (i = 1; i < 20; i++) {
+      newData.push({ key: i + current });
     }
-    this.setState( { data: [...this.state.data, ...newData], last: current + 19} )
+    this.setState({
+      data: [...this.state.data, ...newData],
+      last: current + 19
+    });
   };
-
 
   render() {
     return (
-      <FlatList
-        style = { itemStyle }
-        data = {this.state.data}
-        renderItem={this._renderItem}
-        onEndReached = {this._loadMore}
-        onEndReachedThreshold = {50}
-        horizontal = {true}
-        removeClippedSubviews = {true}
-        getItemLayout = {this.getItemLayout}
-        decelerationRate = {0}
-        snapToInterval = {itemWidth}
-        snapToAlignment = "center"
-        showsHorizontalScrollIndicator = {false}
-        initialScrollIndex = {500}
-      />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <FlatList
+            style={itemStyle}
+            data={this.state.data}
+            renderItem={this._renderItem}
+            onEndReached={this._loadMore}
+            onEndReachedThreshold={50}
+            horizontal={true}
+            removeClippedSubviews={true}
+            getItemLayout={this.getItemLayout}
+            decelerationRate={0}
+            snapToInterval={itemWidth}
+            snapToAlignment="center"
+            showsHorizontalScrollIndicator={false}
+            initialScrollIndex={500}
+          />
+        </View>
+        <View style={{ flex: 0.75 }}>
+          <Agenda />
+        </View>
+      </View>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   itemStyle: {
-    display: "flex",
+    display: 'flex',
     marginTop: 30
-  },
+  }
 });
 
 const { itemStyle } = styles;
-
 
 export default Calendar;
