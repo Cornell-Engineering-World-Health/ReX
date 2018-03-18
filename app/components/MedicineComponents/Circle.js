855f634103a85d84cea8619c96e620ed6942cb42
KatyVoor <katyvoor@gmail.com>
2018-03-18 14:53:15 -0400

    woot

--- app/components/MedicineComponents/Circle.js
+++ app/components/MedicineComponents/Circle.js
@@ -9,7 +9,7 @@ class Circle extends Component {
     super(props);
   
   this.state = {
-    meds: [[0,0,0,0,0],[1,1],[0,0], [1,1,1,1]],
+    meds: [[0,0,0,0,0],[0,0],[0,0], [0,0,0,0]],
     colors:['#F46','#7FDECB80','#F46','#7FDECB80','#F46','#7FDECB80','#F46','#7FDECB80'],
     data : [ 0,100, 0,100,0,100,0,100],
   }
@@ -54,7 +54,7 @@ class Circle extends Component {
         }))
 
     return (
-        <View>
+        <View flex = {1}>
         <View style = {styles.header}>
         <Text style = {styles.headerText}>
             Monday
@@ -63,84 +63,48 @@ class Circle extends Component {
             March 12
         </Text>
         </View>
-        <View>
-            <View flexDirection = 'row' style = {styles.images2}>
+    
+         <FlatList
+         data = {[0]}
+         renderItem = {({ item, index }) => {
+            return (
+            <View>
+             <View>
+            <View flexDirection = 'row' style = {styles.images1}>
                 <Image marginRight = {70} marginTop= {10} source = {require('../Resources/night2.png')}/>
                 <Image source = {require('../Resources/icons8-sunrise-50.png')}/> 
             </View>
-            <View flexDirection = 'row' style = {styles.images1}>
+            <View flexDirection = 'row' style = {styles.images2}>
                 <Image marginRight= {70} source = {require('../Resources/icons8-sunset-50.png')}/>
                 <Image marginTop = {10} source = {require('../Resources/icons8-sun-50.png')}/> 
             </View>
-
-     
         </View>
         <View style = {styles.plusUp} />
         <View style = {styles.plusUp2} />
         <View style = {styles.plusSide} />
         <View style = {styles.plusSide2} />
         <PieChart
+            paddingBottom = {150}
             sort = {(a,b) => 0 }
             padAngle = {0}
             innerRadius= {"90%"}
-            style={ { height: 270,  marginTop: 100} }
+            style={ { height: 270, marginTop: 30} }
             data={ pieData }
-        />
-        {/*<TouchableOpacity style = {styles.button}
-        style = {{alignSelf: 'center'}}
-        onPress= {() => this.updateArray(1)} 
-        >
-            <Text> Press Me </Text>
-        </TouchableOpacity >
-        <TouchableOpacity style = {styles.button}
-        style = {{alignSelf: 'center'}}
-        onPress= {() => this.updateMeds(0,0)} 
-        >
-        <Text> Morning 1</Text>
-        </TouchableOpacity>
-        
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(0,1)} 
-         >
-         <Text> Morning 2 </Text>
-         </TouchableOpacity>
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(0,2)} 
-         >
-         <Text> Morning 3 </Text>
-         </TouchableOpacity>
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(0,3)} 
-         >
-         <Text > Morning 4 </Text>
-         </TouchableOpacity>
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(0,4)} 
-         >
-         <Text > Morning 5 </Text>
-         </TouchableOpacity>
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(2,0)} 
-         >
-         <Text > Evening 1 </Text>
-         </TouchableOpacity>
-         <TouchableOpacity style = {styles.button}
-         style = {{alignSelf: 'center'}}
-         onPress= {() => this.updateMeds(2,1)} 
-         >
-         <Text > Evening 2 </Text>
-         </TouchableOpacity> */}
-         <FlatList
-         data = {[0]}
-         renderItem = {({ item, index }) => {
-            return (
-            <View>
+        />    
             <MedicineCard
+            checked = {this.state.meds[0][0]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(0, 0)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+            checked = {this.state.meds[0][1]}
             title = "My name i"
             timeStamp = "4:20 AM"
             note1 = "My name iSS eashan"
@@ -152,6 +116,7 @@ class Circle extends Component {
             buttonActive = {true}
             />
            <MedicineCard
+            checked = {this.state.meds[0][2]}
             title = "My name i"
             timeStamp = "4:20 AM"
             note1 = "My name iSS eashan"
@@ -162,7 +127,8 @@ class Circle extends Component {
             onPress = {() => this.updateMeds(0, 2)}
             buttonActive = {true}
             />
-           <MedicineCard
+             <MedicineCard
+            checked = {this.state.meds[0][3]}
             title = "My name i"
             timeStamp = "4:20 AM"
             note1 = "My name iSS eashan"
@@ -172,7 +138,116 @@ class Circle extends Component {
             clicked = {true}
             onPress = {() => this.updateMeds(0, 3)}
             buttonActive = {true}
+            />
+           <MedicineCard
+           checked = {this.state.meds[0][4]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(0, 4)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+            checked = {this.state.meds[1][0]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(1, 0)}
+            buttonActive = {true}
             /> 
+             <MedicineCard
+            checked = {this.state.meds[1][1]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(1, 1)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+           checked = {this.state.meds[2][0]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(2, 0)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+           checked = {this.state.meds[2][1]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(2, 1)}
+            buttonActive = {true}
+            />
+             <MedicineCard
+             checked = {this.state.meds[3][0]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(3, 0)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+           checked = {this.state.meds[3][1]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(3, 1)}
+            buttonActive = {true}
+            />
+           <MedicineCard
+           checked = {this.state.meds[3][2]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(3, 2)}
+            buttonActive = {true}
+            />  
+            <MedicineCard
+            checked = {this.state.meds[3][3]}
+            title = "My name i"
+            timeStamp = "4:20 AM"
+            note1 = "My name iSS eashan"
+            note2 = "Im very tired"
+            cardData = {constants.HEADACHE}
+            backgroundColor = "#ffff"
+            clicked = {true}
+            onPress = {() => this.updateMeds(3, 3)}
+            buttonActive = {true}
+            />
+              
             </View>
             );
          }}
@@ -184,17 +259,19 @@ class Circle extends Component {
 }
 const styles = StyleSheet.create({
     header: {
-        marginTop: 20
+        marginTop: 20,
+        marginBottom: 0,
     },
     headerText: {
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: '24',
+        fontSize: 24,
+        margin: 0,
     },
     headerText2: {
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: '19.5',
+        fontSize: 19.5,
     },
     button: {
         marginTop: 20,
@@ -202,20 +279,20 @@ const styles = StyleSheet.create({
     },
     images1: {
         position: 'absolute',
-        top: 250,
+        top: 70,
         left:100,
     },
     images2:{
         position: 'absolute',
         left: 100,
-        top: 150,
+        top: 180,
     },
     plusUp: {
         position: 'absolute',
         height: 13,
         width: 3,
         left: 185.5,
-        top: 88,
+        top: 18,
         backgroundColor: 'grey'
     },
     plusUp2: {
@@ -223,15 +300,17 @@ const styles = StyleSheet.create({
         height: 13,
         width: 3,
         left: 184.5,
-        top: 370,
-        backgroundColor: 'grey'
+        top: 300,
+        backgroundColor: 'grey',
+        paddingBottom: 10,
+
     },
     plusSide: {
         position: 'absolute',
         height: 3,
         width: 13,
         left: 40,
-        top: 247,
+        top: 177,
         backgroundColor: 'grey'
     },
     plusSide2: {
@@ -239,7 +318,7 @@ const styles = StyleSheet.create({
         height: 3,
         width: 13,
         left: 322,
-        top: 247,
+        top: 177,
         backgroundColor: 'grey'
     }
 })
