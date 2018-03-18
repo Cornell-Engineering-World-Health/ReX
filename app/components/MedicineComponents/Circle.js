8c20cfef1a7a0c86e67001f2673310f799729c39
splashofcrimson <splashofcrimson@gmail.com>
2018-03-18 11:07:23 -0700

    circles icons and stuf

--- app/components/MedicineComponents/Circle.js
+++ app/components/MedicineComponents/Circle.js
@@ -1,6 +1,8 @@
-import {TouchableOpacity, View, Text, StyleSheet, Animated} from 'react-native'
 import { PieChart } from 'react-native-svg-charts'
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native'
 import React, { Component } from 'react'
+import constants from '../Resources/constants';
+import MedicineCard from '../Card/MedicineCard'
 
 class Circle extends Component {
   constructor(props) {
@@ -53,6 +55,30 @@ class Circle extends Component {
 
     return (
         <View>
+        <View style = {styles.header}>
+        <Text style = {styles.headerText}>
+            Monday
+        </Text>
+        <Text style = {styles.headerText2}>
+            March 12
+        </Text>
+        </View>
+        <View>
+            <View flexDirection = 'row' style = {styles.images2}>
+                <Image marginRight = {70} marginTop= {10} source = {require('../Resources/night2.png')}/>
+                <Image source = {require('../Resources/icons8-sunrise-50.png')}/> 
+            </View>
+            <View flexDirection = 'row' style = {styles.images1}>
+                <Image marginRight= {70} source = {require('../Resources/icons8-sunset-50.png')}/>
+                <Image marginTop = {10} source = {require('../Resources/icons8-sun-50.png')}/> 
+            </View>
+
+     
+        </View>
+        <View style = {styles.plusUp} />
+        <View style = {styles.plusUp2} />
+        <View style = {styles.plusSide} />
+        <View style = {styles.plusSide2} />
         <PieChart
             sort = {(a,b) => 0 }
             padAngle = {0}
@@ -60,7 +86,7 @@ class Circle extends Component {
             style={ { height: 270,  marginTop: 100} }
             data={ pieData }
         />
-        <TouchableOpacity style = {styles.button}
+        {/*<TouchableOpacity style = {styles.button}
         style = {{alignSelf: 'center'}}
         onPress= {() => this.updateArray(1)} 
         >
@@ -108,15 +134,108 @@ class Circle extends Component {
          onPress= {() => this.updateMeds(2,1)} 
          >
          <Text > Evening 2 </Text>
-         </TouchableOpacity>
+         </TouchableOpacity> */}
+         <FlatList
+         data = {[0, 0, 0]}
+         renderItem = {({ item, index }) => {
+             
+         }}
+         <MedicineCard
+         title = "My name i"
+         timeStamp = "4:20 AM"
+         note1 = "My name iSS eashan"
+         note2 = "Im very tired"
+         cardData = {constants.HEADACHE}
+         backgroundColor = "#ffff"
+         clicked = {true}
+         onPress = {() => this.updateMeds(0, 1)}
+         buttonActive = {true}
+         />
+        <MedicineCard
+         title = "My name i"
+         timeStamp = "4:20 AM"
+         note1 = "My name iSS eashan"
+         note2 = "Im very tired"
+         cardData = {constants.HEADACHE}
+         backgroundColor = "#ffff"
+         clicked = {true}
+         onPress = {() => this.updateMeds(0, 2)}
+         buttonActive = {true}
+         />
+        <MedicineCard
+         title = "My name i"
+         timeStamp = "4:20 AM"
+         note1 = "My name iSS eashan"
+         note2 = "Im very tired"
+         cardData = {constants.HEADACHE}
+         backgroundColor = "#ffff"
+         clicked = {true}
+         onPress = {() => this.updateMeds(0, 3)}
+         buttonActive = {true}
+         />
          </View>
     )
   }
 }
 const styles = StyleSheet.create({
+    header: {
+        marginTop: 20
+    },
+    headerText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: '24',
+    },
+    headerText2: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: '19.5',
+    },
     button: {
         marginTop: 20,
         backgroundColor: '#FFF3'
+    },
+    images1: {
+        position: 'absolute',
+        top: 250,
+        left:100,
+    },
+    images2:{
+        position: 'absolute',
+        left: 100,
+        top: 150,
+    },
+    plusUp: {
+        position: 'absolute',
+        height: 13,
+        width: 3,
+        left: 185.5,
+        top: 88,
+        backgroundColor: 'grey'
+    },
+    plusUp2: {
+        position: 'absolute',
+        height: 13,
+        width: 3,
+        left: 184.5,
+        top: 370,
+        backgroundColor: 'grey'
+    },
+    plusSide: {
+        position: 'absolute',
+        height: 3,
+        width: 13,
+        left: 40,
+        top: 247,
+        backgroundColor: 'grey'
+    },
+    plusSide2: {
+        position: 'absolute',
+        height: 3,
+        width: 13,
+        left: 322,
+        top: 247,
+        backgroundColor: 'grey'
     }
 })
 export default Circle
