15276ace4e260a85aef2e9ea5d443c463373fca8
Albert Chung <albertc435@gmail.com>
2018-04-16 02:07:44 -0400

    Homescreen merge master (#18)
    * updated homescreen
    
    * refactor
    
    * binary fill
    
    * styling
    
    * styling
    
    * added fake data for medicine reminders
    
    * finish merge with medicine Data
    
    * revert constants
    
    * added search query for medicines
    
    * modified other queries to not pull medication reminders
    
    * added startDate and endDate to initial medication reminder events
    
    * formatted medicine data pull
    
    * home screen medicine logger reads from database
    
    * resetting Calendar dir
    
    * resetting Calendar dir 2
    
    * resetting resources

--- app/components/screens/styles.js
+++ app/components/screens/styles.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import constants from '../Resources/constants';
+import {COLOR} from '../Resources/constants';
 
 const width = Dimensions.get('window').width;
 const circleWidth = width*.8;
 const height = Dimensions.get('window').height;
-const borderW = 2;
+
 
 export default StyleSheet.create({
   topInfo: {
@@ -30,7 +30,7 @@ export default StyleSheet.create({
     padding: 20,
   },
   welcomeText: {
-    color: constants.PrimaryGray,
+    color: COLOR.PrimaryGray,
     fontSize: 30,
   },
   nameText: {
@@ -47,61 +47,12 @@ export default StyleSheet.create({
     alignItems: 'center'
   },
   subHeaderText: {
-    color: constants.PrimaryGray,
+    color: COLOR.PrimaryGray,
     fontSize: 18
-  },
-  medicineViewContainer: {
-    height: circleWidth,
-    width: circleWidth,
-  },
-  medicineViewRow: {
-    flexDirection: 'row'
-  },
-  medicineButton: {
-    width: circleWidth/2,
-    height: circleWidth/2,
-    flexDirection: 'row',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFFFFF',
-    //backgroundColor: MEDICINE_BUTTON_BACKGROUND_COLOR,
-    margin: 0,
   },
   imageStyle: {
     width: 100,
     height: 100
   },
-  topLeftQuadrant:{
-    borderTopLeftRadius: circleWidth,
-    borderWidth: borderW,
-    borderBottomColor: constants.PrimaryGray,
-    borderRightColor: constants.PrimaryGray,
-    borderTopColor: constants.red,
-    borderLeftColor: constants.red
-  },
-  topRightQuadrant:{
-    borderTopRightRadius: circleWidth,
-    borderWidth: borderW,
-    borderTopColor: constants.cyan,
-    borderRightColor: constants.cyan,
-    borderBottomColor: constants.PrimaryGray,
-    borderLeftColor: constants.PrimaryGray,
-  },
-  bottomLeftQuadrant:{
-    borderBottomLeftRadius: circleWidth,
-    borderWidth: borderW,
-    borderTopColor: constants.PrimaryGray,
-    borderRightColor: constants.PrimaryGray,
-    borderBottomColor: constants.purple,
-    borderLeftColor: constants.purple
-  },
-  bottomRightQuadrant:{
-    borderBottomRightRadius: circleWidth,
-    borderWidth: borderW,
-    borderTopColor: constants.PrimaryGray,
-    borderLeftColor: constants.PrimaryGray,
-    borderBottomColor: constants.blue,
-    borderRightColor: constants.blue
-  }
+
 });
