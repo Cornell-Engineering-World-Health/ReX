747bfafba2ed350844673de55bbe09b868e9f2de
KatyVoor <kev29@cornell.edu>
2018-12-07 15:16:47 -0500

    everything but the db

--- app/screens/Database.js
+++ app/screens/Database.js
@@ -1,3 +1,5 @@
 import { SQLite } from 'expo'
 
-module.exports = SQLite.openDatabase('db20gayyyysadhhhashjdsahh.db')
+let db_id = Math.random().toString(36).substring(7);
+
+module.exports = SQLite.openDatabase('db'+db_id+'.db')
