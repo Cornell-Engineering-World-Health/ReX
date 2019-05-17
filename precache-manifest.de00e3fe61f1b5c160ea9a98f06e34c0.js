e7fe86266e1e4acf2a79164b3a1fe1f642c6823c
Eashan Garg <eg456@cornell.edu>
2019-05-17 00:23:56 -0400

    Updates

---
+++ precache-manifest.de00e3fe61f1b5c160ea9a98f06e34c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d90cc71bf7cb1614c7200e2e28a073",
+    "url": "/ReX/index.html"
+  },
+  {
+    "revision": "0e36f22dfe121355fa94",
+    "url": "/ReX/static/css/main.e8c246c3.chunk.css"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/ReX/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "0e36f22dfe121355fa94",
+    "url": "/ReX/static/js/main.96adf2bc.chunk.js"
+  },
+  {
+    "revision": "b37efb7f2fc5c793977c",
+    "url": "/ReX/static/js/runtime~main.3f42e9c7.js"
+  },
+  {
+    "revision": "f337c20fdf7ccd87705505ed8e760f38",
+    "url": "/ReX/static/media/apple.f337c20f.png"
+  },
+  {
+    "revision": "6a90c8dade9198f42015ee18a823453f",
+    "url": "/ReX/static/media/back_shape.6a90c8da.png"
+  },
+  {
+    "revision": "6db2387a512b1ec0fc5b009016b4103f",
+    "url": "/ReX/static/media/iphone_header_crop.6db2387a.png"
+  }
+]);
