9e23f9b3ee79c83f6cb880d8655dea1384c351c7
splashofcrimson <splashofcrimson@gmail.com>
2019-05-17 00:05:53 -0400

    iteration one of website

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
 import './Headline.css'
-import bg from '../../resources/background.png';
+import bg from '../../resources/back_shape.png';
+import apple from '../../resources/apple.png';
 
 const Headline = () => {
     return (
         <section>
             <img src={bg} alt='Background Shape' id='bg'/>
-            <h1 className="header-text">
-            <span>Managing your health has</span>
-            <br/>
-            <span>never been so easy</span>
-            </h1>
-            <p>ReX is a health tracker that gives you</p>
-            <p>complete control over everything from</p>
-            <p>tracking medications to logging symptoms</p>
+            <div className="header-box">
+                <p className="header-text">
+                    <span>Managing your health has</span>
+                    <br/>
+                    <span>never been so easy</span>
+                </p>
+                <p className="body-text">
+                    <span>ReX is the health tracker that gives</span>
+                    <br/>
+                    <span>you complete control over everything from</span>
+                    <br/>
+                    <span>tracking prescriptions to logging symptoms</span>
+                </p>
+            </div>
+            <img src={apple} alt="Apple" id='apple' />
         </section>
     );
 }
--- src/components/Iphone/Iphone.js
+++ src/components/Iphone/Iphone.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './Iphone.css'
-import iphone_header from '../../resources/iphone_header_one.png';
+import iphone_header from '../../resources/iphone_header_crop.png';
 
 const Iphone = () => {
     return (
         <section>
+            <div className="iphone-box">
             <img src={iphone_header} alt='iphone_header' id='iphone_header' />
+            </div>
         </section>
     );
 }
